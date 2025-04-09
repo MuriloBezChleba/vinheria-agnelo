77e65f71b92a8832b07443bf70bcb5991cdc6586
MuriloBezChleba <murilogbc2006@gmail.com>
2025-04-08 22:27:04 -0300

    analise de playlist

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('auth/', views.auth, name='auth'),
+    path('callback/', views.callback, name='callback'),
+    path('analisarPlaylist/', views.analisarPlaylist, name='analisarPlaylist')
 ]
